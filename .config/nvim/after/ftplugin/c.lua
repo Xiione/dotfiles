f9f0e1696e864ba74c7dd73e442c488d1d463ef8
Xiione <hamiltonlwang12@gmail.com>
2023-01-24 13:47:57 -0500

    Add c ftplugin file

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,24 @@
+local status_ok, virt_column = pcall(require, "virt-column")
+if not status_ok then
+	return
+end
+
+virt_column.setup_buffer({ virtcolumn = "80" })
+
+local utils = require("user.lib.utils")
+
+utils.map("n", "<leader>r", function()
+	local cmd = utils.resolve_spaces(vim.fn.expand("%:p:r"))
+	utils.send_cmd(cmd, "%:p:h")
+end)
+
+utils.map("n", "<leader>b", function()
+	local cmd = "g++ " .. utils.resolve_spaces(vim.fn.expand("%:p")) ..
+                " -std=gnu++17" ..
+                " -g" ..
+                " -o " .. utils.resolve_spaces(vim.fn.expand("%:p:r"))
+	utils.send_cmd(cmd, "%:p:h")
+end)
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
