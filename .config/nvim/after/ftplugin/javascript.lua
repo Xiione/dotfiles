0d9526a1b4636886bdd8cdf5aacef0ec10eebf56
Xiione <hamiltonlwang12@gmail.com>
2023-06-29 18:25:39 -0700

    Add javascript ftplugin config

---
+++ .config/nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,9 @@
+local status_ok, virt_column = pcall(require, "virt-column")
+if not status_ok then
+	return
+end
+
+virt_column.setup_buffer({ virtcolumn = "80" })
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
