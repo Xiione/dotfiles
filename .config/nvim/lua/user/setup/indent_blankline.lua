4cd89bc986173c500083895832a69a0e7386c4ce
Xiione <hamiltonlwang12@gmail.com>
2023-07-03 18:12:34 -0700

    Fix no indent markers on blank lines

--- .config/nvim/lua/user/setup/indent_blankline.lua
+++ .config/nvim/lua/user/setup/indent_blankline.lua
@@ -1,16 +1,18 @@
 local status_ok, indent_blankline = pcall(require, "indent_blankline")
 if not status_ok then
-  return
+	return
 end
 
 local utils = require("user.lib.utils")
 
-indent_blankline.setup {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = utils.sidebar_types
-}
+indent_blankline.setup({
+	char = "│",
+    char_blankline = "┆",
+	space_char_blankline = " ",
+	show_trailing_blankline_indent = false,
+	show_first_indent_level = true,
+	use_treesitter = true,
+	show_current_context = true,
+	buftype_exclude = { "terminal", "nofile" },
+	filetype_exclude = utils.sidebar_types,
+})
