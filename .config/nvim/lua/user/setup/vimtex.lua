08e3157cd726e2b7510e336273a54966886634ef
Xiione <hamiltonlwang12@gmail.com>
2023-08-30 18:08:26 -0400

    Change default latexmk engine to lualatex

--- .config/nvim/lua/user/setup/vimtex.lua
+++ .config/nvim/lua/user/setup/vimtex.lua
@@ -1,5 +1,18 @@
 vim.g.maplocalleader = " "
+vim.g.vimtex_mappings_prefix = "t"
 vim.g.vimtex_quickfix_open_on_warning = 0
 
-vim.g.vimtex_view_method = 'sioyek'
+vim.g.vimtex_view_method = "sioyek"
 vim.g.vimtex_format_enabled = 1
+vim.g.vimtex_compiler_latexmk_engines = {
+    ["_"] = "-lualatex",
+    ["pdfdvi"] = "-pdfdvi",
+    ["pdfps"] = "-pdfps",
+    ["pdflatex"] = "-pdf",
+    ["luatex"] = "-lualatex",
+    ["lualatex"] = "-lualatex",
+    ["xelatex"] = "-xelatex",
+    ["context (pdftex)"] = "-pdf -pdflatex=texexec",
+    ["context (luatex)"] = "-pdf -pdflatex=context",
+    ["context (xetex)"] = "-pdf -pdflatex='texexec --xtx'",
+}
