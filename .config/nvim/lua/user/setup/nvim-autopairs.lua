fa90b9f2fef274ee7641384f961ed425dadd6479
Xiione <hamiltonlwang12@gmail.com>
2023-09-07 19:40:53 -0400

    Add text $$ autopair

--- .config/nvim/lua/user/setup/nvim-autopairs.lua
+++ .config/nvim/lua/user/setup/nvim-autopairs.lua
@@ -27,6 +27,8 @@ npairs.setup({
 	},
 })
 
+npairs.add_rule(Rule("$$","$$", {"tex", "latex"}))
+
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
