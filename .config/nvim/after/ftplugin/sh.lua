8bcf36da09b7b4c2f542977382dee9ed97000003
Xiione <hamiltonlwang12@gmail.com>
2024-01-03 21:34:03 -0800

    Add sh ftplugin

---
+++ .config/nvim/after/ftplugin/sh.lua
@@ -0,0 +1,10 @@
+local status_ok, virt_column = pcall(require, "virt-column")
+if not status_ok then
+	return
+end
+
+virt_column.setup_buffer(0, { virtcolumn = "80" })
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+
