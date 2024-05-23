07ad9e3d91f4357e9893e54a31d9c672d894a71f
Xiione <hamiltonlwang12@gmail.com>
2024-05-23 15:36:25 -0700

    Fix unable to scroll in toggleterm

--- .config/nvim/after/ftplugin/toggleterm.lua
+++ .config/nvim/after/ftplugin/toggleterm.lua
@@ -1,2 +1,6 @@
 vim.opt_local.signcolumn = "no"
 vim.opt_local.cursorline = false
+
+local utils = require("user.lib.utils")
+utils.map("n", "<Up>", "<Up>", { silent = true }, vim.api.nvim_get_current_buf())
+utils.map("n", "<Down>", "<Down>", { silent = true }, vim.api.nvim_get_current_buf())
