4214f079d8dd4de18764c2d4403fc92686343d67
Xiione <hamiltonlwang12@gmail.com>
2024-09-26 21:58:14 -0400

    Fix bits/stdc++ not being included by clangd

--- .config/nvim/lua/user/lsp/settings/clangd.lua
+++ .config/nvim/lua/user/lsp/settings/clangd.lua
@@ -1,13 +1,18 @@
 return {
 	cmd = {
 		"clangd",
+        "--enable-config",
 		"--background-index",
-		"--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+		"--query-driver=/opt/homebrew/bin/gcc-14,/opt/homebrew/bin/g++-14",
+		-- "--query-driver=/opt/homebrew/bin/gcc-14,/opt/homebrew/bin/g++-14,/opt/homebrew/opt/llvm/bin/clang,/opt/homebrew/opt/llvm/bin/clang++",
+        -- "--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
 		"--clang-tidy",
 		"--clang-tidy-checks=*",
 		"--cross-file-rename",
 		"--completion-style=detailed",
-		"--header-insertion=never",
+		"--header-insertion=iwyu",
+        "--header-insertion-decorators",
 		"--pch-storage=memory",
+        "--rename-file-limit=0"
 	},
 }
