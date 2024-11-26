099334294e48afb92899dda3ccaa79230b4d1637
Xiione <hamiltonlwang12@gmail.com>
2024-11-25 22:40:20 -0500

    Add svelte ls config

---
+++ .config/nvim/lua/user/lsp/settings/svelte.lua
@@ -0,0 +1,24 @@
+return {
+	settings = {
+		svelte = {
+			plugin = {
+				css = {
+					completions = {
+						emmet = false,
+					},
+				},
+				html = {
+					tagComplete = {
+						enable = false,
+					},
+				},
+				svelte = {
+					defaultScriptLanguage = "ts",
+				},
+			},
+		},
+		emmet = {
+			showExpandedAbbreviation = "never",
+		},
+	},
+}
