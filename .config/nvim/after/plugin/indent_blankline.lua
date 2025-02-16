6bd929274f9c70b572732ca2baf9e839e486ad12
Xiione <hamiltonlwang12@gmail.com>
2025-02-15 20:32:12 -0500

    Update ibl color

--- .config/nvim/after/plugin/indent_blankline.lua
+++ .config/nvim/after/plugin/indent_blankline.lua
@@ -4,17 +4,36 @@ if not status_ok then
 end
 
 local sidebars = require("user.lib.sidebars")
+local colors = require("user.cfg.colors")
+
+local hooks = require("ibl.hooks")
+
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "IblRainbowRed", { fg = colors.nord11 })
+	vim.api.nvim_set_hl(0, "IblRainbowYellow", { fg = colors.nord13 })
+	vim.api.nvim_set_hl(0, "IblRainbowCyan", { fg = colors.nord8 })
+end)
+
+local highlight = {
+	"IblRainbowCyan",
+	"IblRainbowRed",
+	"IblRainbowYellow",
+}
 
 indent_blankline.setup({
-    exclude = {
-        buftypes = { "terminal", "nofile" },
-        filetypes = sidebars.sidebar_types
-    },
-    indent = {
-        char = "▏"
-    },
-    scope = {
-        show_start = false,
-        show_end = false
-    }
+	exclude = {
+		buftypes = { "terminal", "nofile" },
+		filetypes = sidebars.sidebar_types,
+	},
+	indent = {
+		char = "▏",
+        -- highlight = highlight
+	},
+	scope = {
+		show_start = false,
+		show_end = false,
+        highlight = highlight
+	},
 })
