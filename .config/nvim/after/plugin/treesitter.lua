16a4526d401926b1f73249583501033eb7830a60
Xiione <hamiltonlwang12@gmail.com>
2025-03-15 15:54:54 -0700

    fix: luasnip begging for latex parser

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -23,14 +23,15 @@ configs.setup({
 		"tsx",
         "rust",
         "svelte",
+        "latex",
 	}, -- put the language you want in this array
 	-- ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "latex" }, -- List of parsers to ignore installing
+	ignore_install = {}, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		disable = { "css", "latex" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
