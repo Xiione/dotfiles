10b119c20f31063f8348d0e174d69baf0ae89fd4
Xiione <hamiltonlwang12@gmail.com>
2025-04-19 22:58:39 -0400

    fix: horizontal trackpad scroll in toggleterm

--- .config/nvim/after/ftplugin/toggleterm.lua
+++ .config/nvim/after/ftplugin/toggleterm.lua
@@ -1,5 +1,6 @@
 vim.opt_local.signcolumn = "no"
 vim.opt_local.cursorline = false
+vim.opt_local.mousescroll = "ver:1,hor:0"
 
 local utils = require("user.lib.utils")
 utils.map("n", "<Up>", "<Up>", { silent = true }, vim.api.nvim_get_current_buf())
