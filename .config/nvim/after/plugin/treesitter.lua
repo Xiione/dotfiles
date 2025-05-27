269532ce5ff5d9f1620bde0470c8c70bc9fb2a36
Hamilton <hamilton.wang@samsara.com>
2025-05-27 15:13:55 -0700

    fix: remove problematic latex treesitter parser

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -5,47 +5,46 @@
 
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+    return
 end
 
 configs.setup({
     auto_install = true,
-	ensure_installed = {
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"bash",
-		"python",
-		"java",
-		"cpp",
-		"javascript",
-		"typescript",
-		"tsx",
+    ensure_installed = {
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "python",
+        "java",
+        "cpp",
+        "javascript",
+        "typescript",
+        "tsx",
         "rust",
         "svelte",
-        "latex",
-	}, -- put the language you want in this array
-	-- ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = {}, -- List of parsers to ignore installing
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    },                         -- put the language you want in this array
+    -- ensure_installed = "all", -- one of "all" or a list of languages
+    ignore_install = { "latex" }, -- List of parsers to ignore installing
+    sync_install = false,      -- install languages synchronously (only applied to `ensure_installed`)
 
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css", "latex" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
+    highlight = {
+        enable = true,          -- false will disable the whole extension
+        disable = { "css", "latex" }, -- list of language that will be disabled
+    },
+    autopairs = {
+        enable = true,
+    },
+    indent = { enable = true, disable = { "python", "css" } },
 
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<CR>",
-			scope_incremental = "<CR>",
-			node_incremental = "<TAB>",
-			node_decremental = "<S-TAB>",
-		},
-	},
-    modules = {}
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+        },
+    },
+    modules = {},
 })
