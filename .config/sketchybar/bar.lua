6167fe209fb1d1a0750a85a8d9f2a2297e7cdc60
Xiione <hamiltonlwang12@gmail.com>
2025-05-26 21:03:18 -0700

    chore: format sketchubar lua

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,8 +2,9 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  height = 40,
-  color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
+	height = 40,
+	color = colors.bar.bg,
+	padding_right = 2,
+	padding_left = 2,
+	notch_width = 190,
 })
--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -5,45 +5,45 @@ local colors = require("colors")
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
 local cal = sbar.add("item", {
-  icon = {
-    color = colors.white,
-    padding_left = 8,
-    font = {
-      style = settings.font.style_map["Black"],
-      size = 12.0,
-    },
-  },
-  label = {
-    color = colors.white,
-    padding_right = 8,
-    align = "right",
-  },
-  position = "right",
-  update_freq = 30,
-  padding_left = 1,
-  padding_right = 1,
-  background = {
-    color = colors.bg2,
-    border_color = colors.black,
-    border_width = 1
-  },
-  click_script = "open -a 'Calendar'"
+	icon = {
+		color = colors.white,
+		padding_left = 8,
+		font = {
+			style = settings.font.style_map["Black"],
+			size = 12.0,
+		},
+	},
+	label = {
+		color = colors.white,
+		padding_right = 8,
+		align = "right",
+	},
+	position = "right",
+	update_freq = 30,
+	padding_left = 1,
+	padding_right = 1,
+	background = {
+		color = colors.bg2,
+		border_color = colors.black,
+		border_width = 1,
+	},
+	click_script = "open -a 'Calendar'",
 })
 
 -- Double border for calendar using a single item bracket
 sbar.add("bracket", { cal.name }, {
-  background = {
-    color = colors.transparent,
-    height = 30,
-    border_color = colors.grey,
-  }
+	background = {
+		color = colors.transparent,
+		height = 30,
+		border_color = colors.grey,
+	},
 })
 
 -- Padding item required because of bracket
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
 cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-  local hour = os.date("%I")
-  local time = string.format("%d:%s", tonumber(hour), os.date("%M %p"))
-  cal:set({ icon = os.date("%a %b %d"), label = time })
+	local hour = os.date("%I")
+	local time = string.format("%d:%s", tonumber(hour), os.date("%M %p"))
+	cal:set({ icon = os.date("%a %b %d"), label = time })
 end)
