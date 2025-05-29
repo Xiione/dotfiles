58fa6872d6c27b020e99e6a4fa28ba1879e62ba1
Hamilton <hamilton.wang@samsara.com>
2025-05-29 09:36:02 -0700

    feat: copilot setup

---
+++ .config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,34 @@
+local status_ok, copilot = pcall(require, "copilot")
+if not status_ok then
+	return
+end
+
+copilot.setup({
+	panel = {
+		enabled = false,
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		trigger_on_accept = true,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+})
