fb664867ead5eddb083a2d2bdbfb0b4ab0a82802
Xiione <hamiltonlwang12@gmail.com>
2026-02-24 18:52:48 -0500

    fix(avante): remove bad insert mode binds

--- .config/nvim/after/ftplugin/AvanteInput.lua
+++ .config/nvim/after/ftplugin/AvanteInput.lua
@@ -3,7 +3,7 @@ local utils = require("user.lib.utils")
 local opts = { remap = true, silent = true, buffer = true }
 utils.map("i", "<D-i>", "<C-o><D-i>", opts)
 
-utils.map("i", "<C-h>", "<C-o><C-h>", opts)
+-- utils.map("i", "<C-h>", "<C-o><C-h>", opts)
 utils.map("i", "<C-j>", "<C-o><C-j>", opts)
 utils.map("i", "<C-k>", "<C-o><C-k>", opts)
-utils.map("i", "<C-l>", "<C-o><C-l>", opts)
+-- utils.map("i", "<C-l>", "<C-o><C-l>", opts)
