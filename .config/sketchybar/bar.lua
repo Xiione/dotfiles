bf65020965501c8606ece9af7677682f2be4cc62
Xiione <hamiltonlwang12@gmail.com>
2026-05-10 10:54:34 -0700

    fix(yabai): disable animation, shrink bar

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,9 +2,9 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	height = 40,
+	height = 30,
 	color = colors.bar.bg,
-	padding_right = 2,
-	padding_left = 2,
+	padding_right = 3,
+	padding_left = 3,
 	notch_width = 190,
 })
