a303add2cbbdd77d224b2febc48018fc2aefc648
Hamilton Wang <hwang@figma.com>
2026-05-28 10:53:22 -0400

    feat(neogurt): add figma to session chooser defaults

--- .config/nvim/after/plugin/neogurt.lua
+++ .config/nvim/after/plugin/neogurt.lua
@@ -53,6 +53,7 @@ local choose_session = function(startup)
                find ~/Documents/Obsidian -mindepth 0 -maxdepth 2 -type d 2>/dev/null;
                find ~/code -mindepth 0 -maxdepth 2 -type d 2>/dev/null;
                find ~/co -mindepth 0 -maxdepth 1 -type d 2>/dev/null;
+               find ~/figma/figma -mindepth 0 -maxdepth 2 -type d 2>/dev/null;
              end;)"
              ]]
 	local output = vim.fn.system(cmd)
