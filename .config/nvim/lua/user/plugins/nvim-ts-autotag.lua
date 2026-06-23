958e33b6fbd5bdf35c732f1553f0822116b6d166
Hamilton Wang <hwang@figma.com>
2026-06-23 13:33:17 -0400

    fix(nvim-ts-autotag): use new config shape

--- .config/nvim/lua/user/plugins/nvim-ts-autotag.lua
+++ .config/nvim/lua/user/plugins/nvim-ts-autotag.lua
@@ -2,9 +2,11 @@ return {
 	"windwp/nvim-ts-autotag",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
-		-- Defaults
-		enable_close = true, -- Auto close tags
-		enable_rename = true, -- Auto rename pairs of tags
-		enable_close_on_slash = false, -- Auto close on trailing </
+		opts = {
+			-- Defaults
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+		},
 	},
 }
